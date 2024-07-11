4a8790cd45c16ba9f01466d23ed814833693a235
Mohaiminul375 <mohaiminul375@gmail.com>
2024-07-12 00:52:33 +0600

    :delete

--- src/Compenents/Home/Home/TaskTable.jsx
+++ src/Compenents/Home/Home/TaskTable.jsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import { FaEye, FaPen, FaTrash } from 'react-icons/fa6';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React from "react";
+import { FaEye, FaPen, FaTrash } from "react-icons/fa6";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+import Swal from "sweetalert2";
 
-const TaskTable = ({task,idx}) => {
-    console.log(task)
-    // destructuring object
-    const {task_title,task_description,status}=task;
-    return (
-        <tr className='even:bg-[#02EBDD] text-white'>
-        <th>{idx+1}</th>
-        <td>{task_title}</td>
-        <td>{task_description}</td>
-        <td>{status}</td>
-        <td className='flex items-center gap-4'>
-          {/* view button */}
-          <FaEye className='text-orange-800'/>
-          {/* edit button */}
-          <FaPen className='text-orange-800 font-bold'/>
-          {/* delete button */}
-           <FaTrash className='bg-red-600 p-2 text-3xl rounded-md'/>
-        </td>
-      </tr>
-    );
+const TaskTable = ({ task, idx }) => {
+  const axiosSecure = useAxiosSecure();
+  const queryClient = useQueryClient();
+  // destructuring object
+  const { _id, task_title, task_description, status } = task;
+
+  // delete
+  const { mutateAsync } = useMutation({
+    mutationFn: async ({ id }) => {
+      const { data } = await axiosSecure.delete(`/tasks/${id}`);
+      return data;
+    },
+    onSuccess: (data) => {
+      if(data.deletedCount>0){
+        Swal.fire({
+            title: "Deleted!",
+            text: "Your task has been deleted.",
+            icon: "success"
+          });
+         queryClient.invalidateQueries({ queryKey: ["all-task"] });
+      }
+    },
+  });
+  const handleDelete = (id) => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            mutateAsync({ id });
+          
+        }
+      });
+  
+  };
+
+  return (
+    <tr className="even:bg-[#02EBDD] text-white">
+      <th>{idx + 1}</th>
+      <td>{task_title}</td>
+      <td>{task_description}</td>
+      <td>{status}</td>
+      <td className="flex items-center gap-4 cursor-pointer">
+        {/* view button */}
+        <FaEye className="text-orange-800 cursor-pointer" />
+        {/* edit button */}
+        <FaPen className="text-orange-800 font-bold cursor-pointer" />
+        {/* delete button */}
+        <FaTrash
+          onClick={() => handleDelete(_id)}
+          className="bg-red-600 p-2 text-3xl rounded-md"
+        />
+      </td>
+    </tr>
+  );
 };
 
 export default TaskTable;
